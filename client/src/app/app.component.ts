185b35a869ed2c7a523ed2e13c676fc4ba6967cc
Aleh Ivoilau <aleh.ivoilau@resliv.by>
2024-09-26 21:22:11 +0300

    Projects

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -4,11 +4,13 @@ import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from './navigation/navigation.component';
 import { HeaderComponent } from './header/header.component';
 import { ProjectsComponent } from "./projects/projects.component";
+import { SearchComponent } from "./shared/search/search.component";
+import { MembersWidgetComponent } from "./shared/members-widget/members-widget.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavigationComponent, HeaderComponent, ProjectsComponent],
+  imports: [RouterOutlet, NavigationComponent, HeaderComponent, ProjectsComponent, SearchComponent, MembersWidgetComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
